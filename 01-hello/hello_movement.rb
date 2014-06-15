cb07180c49a92ac216b6688912dddd0e86aba09d
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2014-06-15 16:38:17 +0300

    Add Hello Movement example

---
+++ 01-hello/hello_movement.rb
@@ -0,0 +1,41 @@
+require 'gosu'
+
+class GameWindow < Gosu::Window
+  def initialize(width=320, height=240, fullscreen=false)
+    super
+    self.caption = 'Hello Movement'
+    @x = @y = 10
+    @draws = 0
+    @buttons_down = 0
+  end
+
+  def update
+    @x -= 1 if button_down?(Gosu::KbLeft)
+    @x += 1 if button_down?(Gosu::KbRight)
+    @y -= 1 if button_down?(Gosu::KbUp)
+    @y += 1 if button_down?(Gosu::KbDown)
+  end
+
+  def button_down(id)
+    close if id == Gosu::KbEscape
+    @buttons_down += 1
+  end
+
+  def button_up(id)
+    @buttons_down -= 1
+  end
+
+  def needs_redraw?
+    @draws == 0 || @buttons_down > 0
+  end
+
+  def draw
+    @draws += 1
+    @message = Gosu::Image.from_text(
+      self, "[x:#{@x};y:#{@y};draws:#{@draws}]", Gosu.default_font_name, 30)
+    @message.draw(@x, @y, 0)
+  end
+end
+
+window = GameWindow.new
+window.show
