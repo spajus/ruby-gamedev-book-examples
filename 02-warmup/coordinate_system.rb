6ebf742a96d9003c6c2d6196e94f7176663c0cb1
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2014-06-25 09:58:29 +0300

    Reformat and improve coordinate system example

--- 02-warmup/coordinate_system.rb
+++ 02-warmup/coordinate_system.rb
@@ -2,12 +2,16 @@ require 'gosu'
 
 class WorldMap
   attr_accessor :on_screen, :off_screen
+
   def initialize(width, height)
     @images = {}
     (0..width).step(50) do |x|
       @images[x] = {}
       (0..height).step(50) do |y|
-        @images[x][y] = Gosu::Image.from_text($window, "#{x}:#{y}", Gosu.default_font_name, 15)
+        img = Gosu::Image.from_text(
+          $window, "#{x}:#{y}",
+          Gosu.default_font_name, 15)
+        @images[x][y] = img
       end
     end
   end
@@ -16,7 +20,7 @@ class WorldMap
     @on_screen = @off_screen = 0
     @images.each do |x, row|
       row.each do |y, val|
-        if camera.can_view?(x, y, val.width, val.height)
+        if camera.can_view?(x, y, val)
           val.draw(x, y, 0)
           @on_screen += 1
         else
@@ -29,40 +33,46 @@ end
 
 class Camera
   attr_accessor :x, :y, :zoom
+
   def initialize
-    @x = 0
-    @y = 0
+    @x = @y = 0
     @zoom = 1
   end
 
-  def can_view?(x, y, obj_width, obj_height)
+  def can_view?(x, y, obj)
     x0, x1, y0, y1 = viewport
-    (x0-obj_width..x1).include?(x) && (y0-obj_height..y1).include?(y)
+    (x0 - obj.width..x1).include?(x) &&
+      (y0 - obj.height..y1).include?(y)
   end
 
   def viewport
-    x0 = @x-($window.width/2)/@zoom
-    x1 = @x+($window.width/2)/@zoom
-    y0 = @y-($window.height/2)/@zoom
-    y1 = @y+($window.height/2)/@zoom
+    x0 = @x - ($window.width / 2)  / @zoom
+    x1 = @x + ($window.width / 2)  / @zoom
+    y0 = @y - ($window.height / 2) / @zoom
+    y1 = @y + ($window.height / 2) / @zoom
     [x0, x1, y0, y1]
   end
 
   def to_s
-    "FPS: #{Gosu.fps}. #{@x}:#{@y} @ #{'%.2f' % @zoom}"
+    "FPS: #{Gosu.fps}. " <<
+      "#{@x}:#{@y} @ #{'%.2f' % @zoom}. " <<
+      'WASD to move, arrows to zoom.'
   end
 
   def draw_crosshair
-    $window.draw_line(@x - 10, @y, Gosu::Color::RED,
-                      @x + 10, @y, Gosu::Color::RED, 100)
-    $window.draw_line(@x, @y - 10, Gosu::Color::RED,
-                      @x, @y + 10, Gosu::Color::RED, 100)
+    $window.draw_line(
+      @x - 10, @y, Gosu::Color::YELLOW,
+      @x + 10, @y, Gosu::Color::YELLOW, 100)
+    $window.draw_line(
+      @x, @y - 10, Gosu::Color::YELLOW,
+      @x, @y + 10, Gosu::Color::YELLOW, 100)
   end
 end
 
 
 class GameWindow < Gosu::Window
   SPEED = 10
+
   def initialize
     super(800, 600, false)
     $window = self
@@ -84,20 +94,35 @@ class GameWindow < Gosu::Window
     @camera.x += SPEED if button_down?(Gosu::KbD)
     @camera.y -= SPEED if button_down?(Gosu::KbW)
     @camera.y += SPEED if button_down?(Gosu::KbS)
+
     zoom_delta = @camera.zoom > 0 ? 0.01 : 1.0
-    @camera.zoom -= zoom_delta if button_down?(Gosu::KbUp)
-    @camera.zoom += zoom_delta if button_down?(Gosu::KbDown)
+
+    if button_down?(Gosu::KbUp)
+      @camera.zoom -= zoom_delta
+    end
+    if button_down?(Gosu::KbDown)
+      @camera.zoom += zoom_delta
+    end
     self.caption = @camera.to_s
   end
 
   def draw
-    translate(-@camera.x+width/2, -@camera.y+height/2) do
+    off_x = -@camera.x + width / 2
+    off_y = -@camera.y + height / 2
+    cam_x = @camera.x
+    cam_y = @camera.y
+    translate(off_x, off_y) do
       @camera.draw_crosshair
-      scale(@camera.zoom, @camera.zoom, @camera.x, @camera.y) do
+      zoom = @camera.zoom
+      scale(zoom, zoom, cam_x, cam_y) do
         @map.draw(@camera)
       end
     end
-    Gosu::Image.from_text(self, "Objects on/off screen: #{@map.on_screen}/#{@map.off_screen}", Gosu.default_font_name, 30).draw(10, 10, 1)
+    info = 'Objects on/off screen: ' <<
+      "#{@map.on_screen}/#{@map.off_screen}"
+    info_img = Gosu::Image.from_text(
+      self, info, Gosu.default_font_name, 30)
+    info_img.draw(10, 10, 1)
   end
 end
 
