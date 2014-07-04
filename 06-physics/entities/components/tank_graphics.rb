a8974e741e3dd64bab063963dbc31078d2b3677f
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2014-07-04 06:27:17 +0300

    Rewrite tank bounding box method

--- 06-physics/entities/components/tank_graphics.rb
+++ 06-physics/entities/components/tank_graphics.rb
@@ -1,4 +1,11 @@
 class TankGraphics < Component
+  DEBUG_COLORS = [
+    Gosu::Color::RED,
+    Gosu::Color::BLUE,
+    Gosu::Color::YELLOW,
+    Gosu::Color::WHITE
+  ]
+
   def initialize(game_object)
     super(game_object)
     @body = units.frame('tank1_body.png')
@@ -22,18 +29,13 @@ class TankGraphics < Component
   end
 
   def draw_bounding_box
-    colors = [
-      Gosu::Color::RED,
-      Gosu::Color::BLUE,
-      Gosu::Color::YELLOW,
-      Gosu::Color::WHITE
-    ]
     i = 0
     object.box.each_slice(2) do |x, y|
+      color = DEBUG_COLORS[i]
       $window.draw_triangle(
-        x - 3, y - 3, colors[i],
-        x, y, colors[i],
-        x + 3 , y - 3, colors[i],
+        x - 3, y - 3, color,
+        x,     y,     color,
+        x + 3, y - 3, color,
         100)
       i = (i + 1) % 4
     end
