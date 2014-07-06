9f668ad4dad0d8d1ec63c4d2b2d03d9c50fb87e2
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2014-07-06 15:04:47 +0300

    Improve performance of #nearby

--- 08-ai/entities/object_pool.rb
+++ 08-ai/entities/object_pool.rb
@@ -7,9 +7,11 @@ class ObjectPool
 
   def nearby(object, max_distance)
     @objects.select do |obj|
-      distance = Utils.distance_between(
-        obj.x, obj.y, object.x, object.y)
-      obj != object && distance < max_distance
+      obj != object &&
+        (obj.x - object.x).abs < max_distance &&
+        (obj.y - object.y).abs < max_distance &&
+        Utils.distance_between(
+          obj.x, obj.y, object.x, object.y) < max_distance
     end
   end
 end
